44dd3e91d175850857759054a763ce9b598bc7f8
Rafael Ocariz <rafael.ocariz@twincats.ca>
2025-03-17 10:26:16 -0400

    added hardcoded tolerances

--- Source/TCMath/Private/Blueprint/ExponentialDecayInterpBpUtils.cpp
+++ Source/TCMath/Private/Blueprint/ExponentialDecayInterpBpUtils.cpp
@@ -8,21 +8,41 @@
 
 float UExponentialDecayInterpBpUtils::FExponentialDecayInterp(float Current,float Target, float DeltaTime, float InterpSpeed)
 {
+	if ((Current - Target) < 0.1f)
+	{
+		return Target;
+	}
+	
 	return TC::Math::ExponentialDecayInterp<float>(Current, Target, DeltaTime, InterpSpeed);
 }
 
 FVector UExponentialDecayInterpBpUtils::VExponentialDecayInterp(const FVector& Current, const FVector& Target, float DeltaTime, float InterpSpeed)
 {
+	if ((Current - Target).Length() < 0.5f)
+	{
+		return Target;
+	}
+	
 	return TC::Math::ExponentialDecayInterp<FVector>(Current, Target, DeltaTime, InterpSpeed);
 }
 
 FVector2D UExponentialDecayInterpBpUtils::V2DExponentialDecayInterp(const FVector2D& Current, const FVector2D& Target, float DeltaTime, float InterpSpeed)
 {
+	if ((Current - Target).Length() < 0.5f)
+	{
+		return Target;
+	}
+	
 	return TC::Math::ExponentialDecayInterp<FVector2D>(Current, Target, DeltaTime, InterpSpeed);
 }
 
 FRotator UExponentialDecayInterpBpUtils::RExponentialDecayInterp(const FRotator& Current, const FRotator& Target, float DeltaTime, float InterpSpeed)
 {
+	if ((Current - Target).IsNearlyZero(1.0f))
+	{
+		return Target;
+	}
+	
 	float Alpha = TC::Math::ExponentialDecayInterp<float>(0.0f, 1.0f, InterpSpeed, DeltaTime);
 	// Using RLerp prevents gimbal lock by properly interpolating rotation values
 	return UKismetMathLibrary::RLerp(Current, Target, Alpha, true);
